3efa3c585c28778a614f40b8dda3a9387ce3e1d3
Yuri Koster <yurikoster1@gmail.com>
2016-11-22 04:53:12 -0200

    added images from Breno´s email and started header and content
    Signed-off-by: Yuri Koster <yurikoster1@gmail.com>

--- WordPress/theme/bemabordo/front-page.php
+++ WordPress/theme/bemabordo/front-page.php
@@ -2,5 +2,19 @@
 include_once ("include/lang_detect.php");
 ?>
 <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory');?>/css/front-page.css">
+<div id="background"></div>
+<div id="content">
+<div id="home" class="container">
+	<span id="textLeft">Fazer o que gostamos<br>
+é o que nos torna <br>
+felizes.<br>
+</span>
+    <span id="textRigth">Trabalhar para tornar <br>
+outras pessoas felizes <br>
+e os negócios sustentáveis,<br> 
+é parte do nosso negócio.<br>
+</span>
+</div>
+</div>
 </body>
 </html>
--- WordPress/theme/bemabordo/header.php
+++ WordPress/theme/bemabordo/header.php
@@ -9,5 +9,39 @@
 include_once ("include/lang_detect.php");
 ?>
 <title><?php echo wp_get_document_title(); ?></title>
+<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory');?>/css/header.css">
+<link href="https://fonts.googleapis.com/css?family=Cabin|Libre+Franklin|PT+Sans:700i" rel="stylesheet">
 </head>
 <body>
+<div id="headerContainer">
+<header>
+<div id="headerTop">
+<span class="centerContainer">
+<span id="logoContainer" class="logoSpan"><img src="<?php bloginfo('template_directory');?>/image/logo.png" width="115" height="87" alt=""/></span>
+<span id="floatContainer">
+<span id="languageContainer">
+<span id="ptContainer"><img src="<?php bloginfo('template_directory');?>/image/pt_active.png" width="33" height="33" alt=""/></span>
+<span id="enContainer"><img src="<?php bloginfo('template_directory');?>/image/en_inactive.png" width="33" height="33" alt=""/></span>
+</span>
+<span id="socialContainer">
+<img src="<?php bloginfo('template_directory');?>/image/facebook.png" width="35" height="35" alt=""/>
+<img src="<?php bloginfo('template_directory');?>/image/linkedin.png" width="35" height="35" alt=""/>
+</span>
+</span>
+</span>
+</div>
+<div id="headerBottom">
+<span class="centerContainer">
+<span id="nameContainer"  class="logoSpan">Bem a Bordo</span>
+<span id="menuContainer">
+<span id="aboutUs">Sobre nós</span>
+<span id="mission">Nossa Missão</span>
+<span id="whoWeAre">Quem Somos</span>
+<span id="whatWeDo">O que fazemos</span>
+<span id="news">Atualidades</span>
+<span id="contact">Contato</span>
+</span>
+</span>
+</div>
+</header>
+</div>
