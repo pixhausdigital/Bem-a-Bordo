d20a87e4a3fadfbab54a246dc2c437b3f09c29ee
Yuri Koster <yurikoster1@gmail.com>
2016-11-23 07:34:28 -0200

    implemented most of static front page fixed #1
    Signed-off-by: Yuri Koster <yurikoster1@gmail.com>

--- WordPress/theme/bemabordo/front-page.php
+++ WordPress/theme/bemabordo/front-page.php
@@ -5,16 +5,191 @@ include_once ("include/lang_detect.php");
 <div id="background"></div>
 <div id="content">
 <div id="home" class="container">
-	<span id="textLeft">Fazer o que gostamos<br>
-é o que nos torna <br>
-felizes.<br>
-</span>
-    <span id="textRigth">Trabalhar para tornar <br>
-outras pessoas felizes <br>
-e os negócios sustentáveis,<br> 
-é parte do nosso negócio.<br>
-</span>
+	<div id="textTopLine">Fazer o que gostamos é o que nos torna felizes.</div>
+    <div id="textBottomLine">Trabalhar para tornar outras pessoas felizes e os negócios sustentáveis, é parte do nosso negócio.
 </div>
+</div>
+<div id="aboutUs" class="container">
+	<div id="aboutUsContentContainer" class="contentContainer">
+    	<div id="aboutUsTitle" class="titlePart">Bem a Bordo</div>
+    
+    	<div id="aboutUsText">
+        <p>“O Mestre na arte da vida faz pouca distinção entre o seu trabalho e o seu lazer, entre a sua mente e o seu corpo, entre a sua educação e a sua recreação, entre o seu amor e a sua religião. Ele dificilmente sabe distinguir um corpo do outro. Ele simplesmente persegue sua visão de excelência em tudo que faz, deixando para os outros a decisão de saber se está trabalhando ou se divertindo. Ele acha que está sempre fazendo as duas coisas simultaneamente.” </p>
+<p>O texto budista acima expressa em sua simplicidade, a política de negócios da Bem a Bordo, contemplando nossa missão, visão, princípios e valores.</p>
+<p>Bem a Bordo – Negócios Sustentáveis foi criada com o objetivo de contribuir para uma sociedade mais justa e equilibrada, onde os negócios prosperam porque são bons e porque devem servir à sociedade.</p>
+<p>Nós, sócios fundadores, temos como filosofia de vida e propósito de existência, gostar tanto do que fazemos que não sabemos se trabalhamos nos divertindo ou, se nos divertimos trabalhando!</p>
+<p>Com essa política de bem viver e “bem trabalhar”, conquistamos uma equipe multidisciplinar de colaboradores, contratados, parceiros e apoiadores que é formada por engenheiros, químicos, advogados, auditores, profissionais de marketing e mídias. Conosco estão também, valorosos profissionais oriundos do quadro de reserva da Marinha do Brasil, de comitês técnicos de organizações públicas e privadas de onde trouxeram larga experiência em suas respectivas áreas de atuação. As relações são estabelecidas com base na ética e na transparência, o que produz resultados altamente satisfatórios e confiáveis.</p>
+<p>Que possamos ser mestres na arte da vida, deixando nossa marca de excelência por onde passarmos.</p>
+
+		</div>
+	</div>
+</div>
+
+<div id="mission" class="container">
+	<div id="missionContentContainer" class="contentContainer">
+    	<div id="missionTitle" class="titlePart">Nossa Missão</div>
+        
+        <div id="missionSpacer"></div>
+        
+        <div id="missionText">
+        	<p>Contribuir na criação, no desenvolvimento e gestão de negócios sustentáveis, onde a eficiência e a produtividade tornam-se diferenciais competitivos; onde o capital humano é valorizado e a satisfação das necessidades do cliente agrega valor ao seu produto ou serviço; onde inteligência e consciência caminham juntas na conquista de resultados criativos, inovadores, responsáveis e éticos; onde a superação dos desafios vem da paixão pelo que fazemos, da motivação por fazer melhor;  da colaboração e do compartilhamento de experiências;  da generosidade e do respeito ao outro.  Trabalhamos com desafios! Porque geram conhecimento, criatividade, motivação e satisfação por produzirem mudanças significativas no ambiente de negócios e na sociedade. Porque contribuem para que as atividades produtivas sejam exercidas cada vez mais de forma íntegra e transparente. Inspiração; transpiração; persistência, encantamento, paixão, conquista, integração é muito do que nos motiva a oferecer soluções para a gestão de negócios sustentáveis.
+</p>
+
+<p> Sabemos que as organizações são compostas por sistemas, processos, máquinas e equipamentos, mas, o que realmente as torna únicas, são as pessoas. Por isso, a Bem a Bordo investe na formação de funcionários mais qualificados e oferece cursos de capacitação e treinamento em diversas áreas e funções, criando condições favoráveis para que funcionários e colaboradores possam desenvolver suas competências, habilidades e atitudes, conectando-os com os valores e diretrizes da organização. Na gestão de negócios, realizamos análise de cenários e tendências para caracterizar os desafios e identificar as oportunidades. Elaboração de planejamento estratégico e assessoramento na execução dos planos e programas. Adequamos as organizações para certificações internacionais, através de implantação de sistemas de gestão. </p>
+
+        </div>
+    </div>
+</div>
+
+<div id="whatWeDo" class="container">
+	<div id="whatWeDoContentContainer" class="contentContainer">
+    	<div id="whatWeDoTitle" class="titlePart">O que Fazemos</div>
+        
+        <div id="whatWeDoFloatersContainer">
+        	<div id="planningFloater" class="floater">
+            	<div id="planningIcon" class="floaterIcon"><img src="<?php bloginfo('template_directory');?>/image/compass.png" alt=""/></div>
+                <div id="planningTitle" class="floaterTitle">Planejamento</div>
+                <div id="planningText">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod temporLorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diamem ipsum dolor sit amet.</div>
+            </div>
+            
+            <div id="advisoryFloater" class="floater">
+            	<div id="advisoryIcon" class="floaterIcon"><img src="<?php bloginfo('template_directory');?>/image/lighthouse.png" alt=""/></div>
+                <div id="advisoryTitle" class="floaterTitle">Assessoria</div>
+                <div id="advisoryText">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod temporLorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diamem ipsum dolor sit amet.</div>
+            </div>
+            
+            <div id="trainingFloater" class="floater">
+            	<div id="trainingIcon" class="floaterIcon"><img src="<?php bloginfo('template_directory');?>/image/knot.png" alt=""/></div>
+                <div id="trainingTitle" class="floaterTitle">Capacitação</div>
+                <div id="trainingText">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod temporLorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diamem ipsum dolor sit amet.</div>
+            </div>
+            
+            <div id="administrationFloater" class="floater">
+            	<div id="administrationIcon" class="floaterIcon"><img src="<?php bloginfo('template_directory');?>/image/cap.png" alt=""/></div>
+                <div id="administrationTitle" class="floaterTitle">Administração</div>
+                <div id="administrationText">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod temporLorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diamem ipsum dolor sit amet.</div>
+            </div>
+            
+              <div id="managementFloater" class="floater">
+            	<div id="managementIcon" class="floaterIcon"><img src="<?php bloginfo('template_directory');?>/image/helm.png" alt=""/></div>
+                <div id="managementTitle" class="floaterTitle">Gestão</div>
+                <div id="managementText">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod temporLorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diamem ipsum dolor sit amet.</div>
+            </div>
+            
+            <div id="habilitationFloater" class="floater">
+            	<div id="habilitationIcon" class="floaterIcon"><img src="<?php bloginfo('template_directory');?>/image/ship.png" alt=""/></div>
+                <div id="habilitationTitle" class="floaterTitle">Habilitação</div>
+                <div id="habilitationText">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod temporLorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diamem ipsum dolor sit amet.</div>
+            </div>
+            
+        </div>
+	</div>
+</div>
+
+<div id="whoWeAre" class="container">
+	<div id="whoWeAreContentContainer" class="contentContainer">
+    	<div id="whoWeAreTitle" class="titlePart">Quem Somos</div>
+        
+        	<div id="whoWeAreFloatersContainer">
+        		<div id="person1" class="person">
+            		<span class="personPhoto"><img src="<?php bloginfo('template_directory');?>/image/tania.png" alt=""/></span>
+                    <span class="personText">
+                    	<div class="personName">Tania Fereira</div>
+                        <span class="personSocialContainer"><a href=""><img src="<?php bloginfo('template_directory');?>/image/weAreFacebook.png" alt=""/></a><a href=""><img src="<?php bloginfo('template_directory');?>/image/weAreLinkedin.png" alt=""/></a></span>
+                        <div class="personJob">Diretora Técnica</div>
+                        <div class="personDescription">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod temporLorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod temporLorem ipsum dolor sit amet, consetetur sadip.</div>
+                    </span>
+            	</div>
+                
+        		<div id="person2" class="person">
+            		<span class="personPhoto"><img src="<?php bloginfo('template_directory');?>/image/breno.png" alt=""/></span>
+                    <span class="personText">
+                    	<div class="personName">Breno Bezinelli</div>
+                        <span class="personSocialContainer"><a href=""><img src="<?php bloginfo('template_directory');?>/image/weAreFacebook.png" alt=""/></a><a href=""><img src="<?php bloginfo('template_directory');?>/image/weAreLinkedin.png" alt=""/></a></span>
+                        <div class="personJob">Diretor Executivo</div>
+                        <div class="personDescription">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod temporLorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod temporLorem ipsum dolor sit amet, consetetur sadip.</div>
+                    </span>
+            	</div>
+                
+        		<div id="person3" class="person">
+            		<span class="personPhoto"><img src="<?php bloginfo('template_directory');?>/image/woman.png" alt=""/></span>
+                    <span class="personText">
+                    	<div class="personName">Estela Almeida</div>
+                        <span class="personSocialContainer"><a href=""><img src="<?php bloginfo('template_directory');?>/image/weAreFacebook.png" alt=""/></a><a href=""><img src="<?php bloginfo('template_directory');?>/image/weAreLinkedin.png" alt=""/></a></span>
+                        <div class="personJob">Gestora Ambiental</div>
+                        <div class="personDescription">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod temporLorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod temporLorem ipsum dolor sit amet, consetetur sadip.</div>
+                    </span>
+            	</div>
+                
+        		<div id="person4" class="person">
+            		<span class="personPhoto"><img src="<?php bloginfo('template_directory');?>/image/man.png" alt=""/></span>
+                    <span class="personText">
+                    	<div class="personName">Deraldo Costa</div>
+                        <span class="personSocialContainer"><a href=""><img src="<?php bloginfo('template_directory');?>/image/weAreFacebook.png" alt=""/></a><a href=""><img src="<?php bloginfo('template_directory');?>/image/weAreLinkedin.png" alt=""/></a></span>
+                        <div class="personJob">Tenente da Marinha do Brasil</div>
+                        <div class="personDescription">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod temporLorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod temporLorem ipsum dolor sit amet, consetetur sadip.</div>
+                    </span>
+            	</div>
+                
+        		<div id="person5" class="person">
+            		<span class="personPhoto"><img src="<?php bloginfo('template_directory');?>/image/woman.png" alt=""/></span>
+                    <span class="personText">
+                    	<div class="personName">Fabiana Melo</div>
+                        <span class="personSocialContainer"><a href=""><img src="<?php bloginfo('template_directory');?>/image/weAreFacebook.png" alt=""/></a><a href=""><img src="<?php bloginfo('template_directory');?>/image/weAreLinkedin.png" alt=""/></a></span>
+                        <div class="personJob">Auditoria de Sistemas de Gestão</div>
+                        <div class="personDescription">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod temporLorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod temporLorem ipsum dolor sit amet, consetetur sadip.</div>
+                    </span>
+            	</div>
+                
+        		<div id="person6" class="person">
+            		<span class="personPhoto"><img src="<?php bloginfo('template_directory');?>/image/man.png" alt=""/></span>
+                    <span class="personText">
+                    	<div class="personName">Luis Chagas</div>
+                        <span class="personSocialContainer"><a href=""><img src="<?php bloginfo('template_directory');?>/image/weAreFacebook.png" alt=""/></a><a href=""><img src="<?php bloginfo('template_directory');?>/image/weAreLinkedin.png" alt=""/></a></span>
+                        <div class="personJob">Tenente da Marinha do Brasil</div>
+                        <div class="personDescription">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod temporLorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod temporLorem ipsum dolor sit amet, consetetur sadip.</div>
+                    </span>
+            	</div>
+                
+        		<div id="person7" class="person">
+            		<span class="personPhoto"><img src="<?php bloginfo('template_directory');?>/image/man.png" alt=""/></span>
+                    <span class="personText">
+                    	<div class="personName">Marcos Vinicius</div>
+                        <span class="personSocialContainer"><a href=""><img src="<?php bloginfo('template_directory');?>/image/weAreFacebook.png" alt=""/></a><a href=""><img src="<?php bloginfo('template_directory');?>/image/weAreLinkedin.png" alt=""/></a></span>
+                        <div class="personJob">Tenente da Marinha do Brasil</div>
+                        <div class="personDescription">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod temporLorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod temporLorem ipsum dolor sit amet, consetetur sadip.</div>
+                    </span>
+            	</div>
+                
+        		<div id="person8" class="person">
+            		<span class="personPhoto"><img src="<?php bloginfo('template_directory');?>/image/man.png" alt=""/></span>
+                    <span class="personText">
+                    	<div class="personName">Plinio Soares</div>
+                        <span class="personSocialContainer"><a href=""><img src="<?php bloginfo('template_directory');?>/image/weAreFacebook.png" alt=""/></a><a href=""><img src="<?php bloginfo('template_directory');?>/image/weAreLinkedin.png" alt=""/></a></span>
+                        <div class="personJob">Engenheiro Mecânico e de Segurança</div>
+                        <div class="personDescription">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod temporLorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod temporLorem ipsum dolor sit amet, consetetur sadip.</div>
+                    </span>
+            	</div>
+                
+        		<div id="person9" class="person">
+            		<span class="personPhoto"><img src="<?php bloginfo('template_directory');?>/image/woman.png" alt=""/></span>
+                    <span class="personText">
+                    	<div class="personName">Roberta Faraco</div>
+                        <span class="personSocialContainer"><a href=""><img src="<?php bloginfo('template_directory');?>/image/weAreFacebook.png" alt=""/></a><a href=""><img src="<?php bloginfo('template_directory');?>/image/weAreLinkedin.png" alt=""/></a></span>
+                        <div class="personJob">Engenheira Civil</div>
+                        <div class="personDescription">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod temporLorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod temporLorem ipsum dolor sit amet, consetetur sadip.</div>
+                    </span>
+            	</div>
+                
+				
+                
+                
+                            	
+        	</div>
+    </div>
+</div>
+
+
 </div>
 </body>
 </html>
